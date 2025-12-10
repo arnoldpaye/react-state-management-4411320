968c105f0e56176bd94cf1b54c969e6cda3d7d25
Arnold Paye <arnoldpayet@gmail.com>
2025-12-10 10:40:10 -0400

    create store

--- exercise-files/05_Zustand/05_02/starter/src/store.js
+++ exercise-files/05_Zustand/05_02/starter/src/store.js
@@ -4,21 +4,19 @@ const useCounter = create((set, get) => ({
   count: 0,
   increment: () => {
     set(() => ({
-      count: get().input ? get().count + get().input : get().count + 1,
+      count: get().count + 1,
     }));
   },
   decrement: () => {
-    if (get().count < 1) {
-      return false;
-    }
     set(() => ({
       count: get().count - 1,
     }));
   },
-  reset: () =>
+  reset: () => {
     set(() => ({
       count: 0,
-    })),
+    }));
+  },
 }));
 
 export default useCounter;
